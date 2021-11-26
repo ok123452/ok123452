d993c9b671ba96781fcb7c12569ea8f4c72825bb
ok123452 <95028500+ok123452@users.noreply.github.com>
2021-11-26 13:43:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 #載入linebot所需套件
 from flask import Flask, request, abort
-from linebot limport Linebot, Webhookhander
+from linebot import Linebot, Webhookhander
 from linebot.exceptions import InvalidSignatureError
 from linebot.modes import MessageEvent, TextMessage, TextSendMessage
 
 import confing
 
-app = Flas(_neme_)
+app = Flask(_neme_)
 
-#讀confing
-confing = confingparser.ConfingParser()
-confing.read('confing.ini)
+
 
 #機器人資料
 line_bot_api = LinebotApi(confing.get('3bG+w2hW7BANHLOhCw2N10C9qCFW70XcoYcx29wEembHnDUqVG4E7tBC+mmu9d6geT5d11qUHM3JiONllp0/xoVS34O5kME9cahaUWwRtZJxQ7wPbp7mJhMBpYidkRnlRXpPmGsJV2wF2EkfOMx9fAdB04t89/1O/w1cDnyilFU=')
@@ -18,7 +16,7 @@ handler = Webhookhander(confing.get('f2b5f68345dde00bc44e12b1be42ed6b')
 
 #接收通知
 @app.route("/callback", methods=['POST'])
-def   callbac():
+def   callback():
       signature = request.headers['X-Line-signature']
                         
       body = request.get_data(as_text=True)
